bbaffd7ca1986089f2ec073e18e5b94ec064468f
minwg1021 <minwg1021@gmail.com>
2018-11-13 19:36:02 +0900

    check.c / trans.c revised[1]

--- check.c
+++ check.c
@@ -4,12 +4,16 @@ int is_valid(char *op, char *args)
 {
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
+	int i=0;
+	char *src;
+	char *des;
 
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
-	
+	src = strtok(args,",");
+	des = strtok(NULL,",");
+
+	if(src[i] =='(' && des[i] =='(') return 0;
+	else if(des[i] =='$') return 0;
+	else if(src[i] == '$' && des[i] == '$') return 0;
+	else return 1;
 	return 1;
 }
