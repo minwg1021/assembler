cdaf9f1111af41e7a4f43eca08307911d979c814
minwg1021 <minwg1021@gmail.com>
2018-11-13 20:02:36 +0900

    check.c revised[2]

--- check.c
+++ check.c
@@ -5,15 +5,17 @@ int is_valid(char *op, char *args)
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
 	int i=0;
+	char arr[100];
+
 	char *src;
 	char *des;
 
-	src = strtok(args,",");
-	des = strtok(NULL,",");
+	strcpy(arr,args);
+	src = strtok(arr,",");
+	des = strtok(NULL,"\n");
 
 	if(src[i] =='(' && des[i] =='(') return 0;
 	else if(des[i] =='$') return 0;
 	else if(src[i] == '$' && des[i] == '$') return 0;
 	else return 1;
-	return 1;
 }
